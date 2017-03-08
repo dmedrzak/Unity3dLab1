e4b7ca189f0047ace3408d106013af0c96bea14f
dmedrzak <daniel.medrzak@gmail.com>
2017-03-08 07:58:30 +0100

    Unity3dLab1
    First game

---
+++ lab1/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public Transform sphere;
+
+	void Update ()
+    {
+        //pobieranie komponent fizyki z naszej kuli
+        Rigidbody rigidbody = sphere.GetComponent<Rigidbody>();
+
+        //obliczenie nowej pozycji dla kamery 
+        Vector3 vector = new Vector3(0, 5f, -7f);
+        //pobieramy prędkość kuli        
+        float velocity = rigidbody.velocity.sqrMagnitude;
+        //zmieniamy odległość kamery tak aby zależała od prędkości kuli
+        vector *= (1f+velocity / 250);
+
+        //obliczamy nową pozycje kamery
+        Vector3 newPosition = sphere.position + vector;
+
+
+        //Za pomocą transformacji Lerp nadajemy płynne poruszanie się kamerze
+        transform.position = Vector3.Lerp(transform.position,newPosition,Time.deltaTime*2f);
+
+        //zmuszamy kamere aby patrzyła w środek naszej kuli
+        transform.LookAt(sphere);
+	}
+}
---
+++ lab1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10f;
+    private int count;
+    public Text countText;
+    public Text winText;
+    void Start()
+    {
+        count = 0;
+        SetCountText();      
+    }
+
+	void Update ()
+    {
+        //pobranie fizyki komponentu kuli
+        Rigidbody rigidbody = transform.GetComponent<Rigidbody>();
+        //tworzymy zmienną kierunku zainicjalizowaną wartością zero,
+        //oznacza to że jeżeli nie nacisniemy zadnego klawisza to nienastąpi zmiana kierunku ruchu kuli
+        Vector3 direction = Vector3.zero;
+
+        //zmiana kierunku ruchu 
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = -Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction = Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            direction = -Vector3.forward;
+        }
+
+        rigidbody.AddTorque(direction * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "PickUp")
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+        if (other.gameObject.tag == "PickUp2")
+        {
+            other.gameObject.SetActive(false);
+            count += 4;
+            SetCountText();
+        }
+    }
+
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 9)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
